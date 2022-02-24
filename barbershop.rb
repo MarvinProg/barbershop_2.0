04e9cce2b535178ba36f474e3e92242ae001fb5a
lomakin <lomakin-as@mail.ru>
2022-02-24 16:22:13 +0300

    update: Add in method POST saves date in file

--- barbershop.rb
+++ barbershop.rb
@@ -11,5 +11,11 @@ post '/' do
 
   @title = "Welcom to the our mini Barbershop!"
   @message = "Hi, my amazin frend, #{@persona_barbershop}. We glad to see you #{@data_visit}"
+
+  file_data = File.open("user_date.txt", "a") do |f|
+  f.write("Persona_barbershop: #{@persona_barbershop}, Phone: #{@phone}, Data_visit: #{@data_visit}\n")
+  f.close
+  end
+
   erb :register_user 
 end
