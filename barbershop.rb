654262ac4727b773ac1c9f2171b3066c59b3e2ac
lomakin <lomakin-as@mail.ru>
2022-02-24 15:48:50 +0300

    update: add in method post variable and visit page

--- barbershop.rb
+++ barbershop.rb
@@ -5,5 +5,11 @@ get '/' do
 end
 
 post '/' do 
-  
+  @persona_barbershop = params[:persona_barbershop]
+  @phone = params[:phone]
+  @data_visit = params[:data_visit]
+
+  @title = "Welcom to the our mini Barbershop!"
+  @message = "Hi, my amazin frend, #{@persona_barbershop}. We glad to see you #{@data_visit}"
+  erb :register_user 
 end
