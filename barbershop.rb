f3454dcc02718c4a2ec46774b60af23b5284e006
lomakin <lomakin-as@mail.ru>
2022-02-25 10:00:00 +0300

    update: change relocate file on ./public and write in GET and POST method

--- barbershop.rb
+++ barbershop.rb
@@ -12,10 +12,16 @@ post '/' do
   @title = "Welcom to the our mini Barbershop!"
   @message = "Hi, my amazin frend, #{@persona_barbershop}. We glad to see you #{@data_visit}"
 
-  file_data = File.open("user_date.txt", "a") do |f|
+  file_data = File.open("./public/user_date.txt", "a") do |f|
   f.write("Persona_barbershop: #{@persona_barbershop}, Phone: #{@phone}, Data_visit: #{@data_visit}\n")
   f.close
   end
 
   erb :register_user 
 end
+
+get '/admin' do 
+  send_file './public/user_date.txt'
+end
+
+
